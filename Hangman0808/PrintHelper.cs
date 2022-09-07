4f4850cf9d81bc9dd6e01ba14b30f55a8bcad40c
Gusts Linkevics <gusts@gst.lv>
2022-09-07 19:37:31 +0300

    Add project files.

---
+++ Hangman0808/PrintHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman0808
+{
+    internal class PrintHelper
+    {
+        public static void PrintWord(string printableWord)
+        {
+            var word = GetFormatedPrintableWord(printableWord);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine(word);
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine();
+        }
+
+        public static void PrintResult(int retryCount, List<string> usedLetters)
+        {
+            Console.WriteLine($"Retry count: {retryCount}");
+
+            Console.WriteLine($"Used letters: {string.Join(",", usedLetters)}");
+        }
+
+        static string GetFormatedPrintableWord(string printableName)
+        {
+            return string.Join(" ", printableName.ToCharArray());
+        }
+    }
+}
---
+++ Hangman0808/Program.cs
@@ -0,0 +1,103 @@
+﻿namespace Hangman0808
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxNumberOfRetries = 6;
+            const string wildCard = "_";
+
+            string[] words = { "cat", "dog", "bird" };
+
+            List<string> usedLetters = new List<string>();
+            int retryCount = 0;
+            Random random = new Random();
+
+            string printableWord = "";
+
+            int index = random.Next(words.Length);
+
+            string wordToGuess = words[index];
+
+            // replace letters with placeholders in printable version
+            foreach (var letter in wordToGuess)
+            {
+                printableWord += wildCard;
+            }
+
+            PrintHelper.PrintWord(printableWord);
+
+            string letterFromUser;
+
+            // game loop
+            while (true)
+            {
+                // get letter from user
+                while (true)
+                {
+                    Console.WriteLine("Please enter a letter:");
+                    letterFromUser = Console.ReadLine();
+
+                    if (string.IsNullOrEmpty(letterFromUser))
+                    {
+                        Console.WriteLine("Please enter a letter!");
+                        continue;
+                    }
+
+                    if (letterFromUser.Length > 1)
+                    {
+                        Console.WriteLine("Please enter only one letter!");
+                        continue;
+                    }
+
+                    break;
+                }
+
+                if (wordToGuess.ToUpper().Contains(letterFromUser.ToUpper()))
+                {
+                    // convert to char array to use iondex for replacing letters
+                    var charArr = printableWord.ToCharArray();
+
+                    // swap _ to actual letter
+                    for (int i = 0; i < wordToGuess.Length; i++)
+                    {
+                        if (wordToGuess[i].ToString().ToUpper() == letterFromUser.ToUpper())
+                        {
+                            charArr[i] = letterFromUser.ToUpper()[0];
+                        }
+                    }
+
+                    // re-assign new value
+                    printableWord = string.Join("", charArr);
+
+                }
+                else
+                {
+                    if (!usedLetters.Contains(letterFromUser.ToUpper()))
+                    {
+                        usedLetters.Add(letterFromUser.ToUpper());
+                    }
+
+                    ++retryCount;
+                }
+
+                PrintHelper.PrintWord(printableWord);
+                PrintHelper.PrintResult(retryCount, usedLetters);
+
+                if(!printableWord.Contains(wildCard))
+                {
+                    Console.WriteLine("You won !!!");
+                    return;
+                }
+
+                if (retryCount >= maxNumberOfRetries)
+                {
+                    Console.WriteLine("Game over");
+                    return; //break;
+                }
+            }
+        }
+
+
+    }
+}
